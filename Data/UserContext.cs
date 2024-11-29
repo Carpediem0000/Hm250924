48d4d3f562e7f0f32522a5313f2edfba7404130b
Ivan Zinovchuk <zinovchukdfuz2@gmail.com>
2024-11-29 16:33:41 +0200

    Add project files.

---
+++ Data/UserContext.cs
@@ -0,0 +1,15 @@
+﻿using Hm250924.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Hm250924.Data
+{
+    public class UserContext : DbContext
+    {
+        public UserContext(DbContextOptions<UserContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
